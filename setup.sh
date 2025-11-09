1924ee1d3f5e933588fc470bee6fff16117ec8f6
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-11-09 18:21:02 -0300

    Adjust iSh to use a newer version of Alpine repos

--- setup.sh
+++ setup.sh
@@ -83,9 +83,11 @@ configure_doas() {
 
 # Use the stardard Alpine repo instead of iSH's
 rm -rf /ish
-echo https://dl-cdn.alpinelinux.org/alpine/v3.14/main > /etc/apk/repositories
-echo https://dl-cdn.alpinelinux.org/alpine/v3.14/community >> /etc/apk/repositories
-apk upgrade & apk fix
+echo https://dl-cdn.alpinelinux.org/alpine/v3.18/main > /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.18/community >> /etc/apk/repositories
+apk add --upgrade apk-tools
+apk upgrade --available
+apk fix
 
 # Configures git
 apk add git
