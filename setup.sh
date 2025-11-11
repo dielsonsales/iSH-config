95c978529b923ba1dd431b0885b943b4ba52a402
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2025-11-11 11:48:20 -0300

    Revert version of Alpine to 3.14 when adding a new user

--- setup.sh
+++ setup.sh
@@ -83,8 +83,8 @@ configure_doas() {
 
 # Use the stardard Alpine repo instead of iSH's
 rm -rf /ish
-echo https://dl-cdn.alpinelinux.org/alpine/v3.18/main > /etc/apk/repositories
-echo https://dl-cdn.alpinelinux.org/alpine/v3.18/community >> /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.14/main > /etc/apk/repositories
+echo https://dl-cdn.alpinelinux.org/alpine/v3.14/community >> /etc/apk/repositories
 apk add --upgrade apk-tools
 apk upgrade --available
 apk fix
